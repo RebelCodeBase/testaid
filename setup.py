647b3f3007b788449097cb9a57b4b4bbd9b365c0
rebelcodebase <rebelcodebase@riseup.net>
2019-07-18 15:36:04 +0200

    fix typo in install_requires

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         url='https://github.com/RebelCodeBase/testaid',
         license='Apache-2.0',
         packages=setuptools.find_packages(),
-        install_requires=['docutils>=0.3', 'testinfra=>3.0.5', 'molecule>=2.20.2'],
+        install_requires=['docutils>=0.3', 'testinfra>=3.0.5', 'molecule>=2.20.2'],
         setup_requires=['pytest-runner'],
         tests_require=['pytest'],
         package_data={
