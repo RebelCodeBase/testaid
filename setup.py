630f5596184c9ae413b871c3bfaa4db9e9cbd91a
rebelcodebase <rebelcodebase@riseup.net>
2019-07-18 15:32:40 +0200

    provide setup configuration

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='testaid',
+        version='0.1',
+        author='RebelCodeBase',
+        author_email='rebelcodebase@riseup.net',
+        description='Fixtures for testinfra and molecule',
+        long_description=open(os.path.join(
+            os.path.dirname(__file__), 'README.rst')).read(),
+        url='https://github.com/RebelCodeBase/testaid',
+        license='Apache-2.0',
+        packages=setuptools.find_packages(),
+        install_requires=['docutils>=0.3', 'testinfra=>3.0.5', 'molecule>=2.20.2'],
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest'],
+        package_data={
+            '': ['*.rst'],
+        },
+        zip_safe=False,
+    )
