2f0749a010575ebc2e60d50e16d314803459ba44
rebelcodebase <rebelcodebase@riseup.net>
2019-07-19 03:20:17 +0200

    add minimal molecule test project

---
+++ test/molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def setup_module(module):
+    print('setup_module')
+    os.system('molecule converge')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
