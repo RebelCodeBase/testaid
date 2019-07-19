957868cee189021c94cc17c7a001e49a48b494f5
rebelcodebase <rebelcodebase@riseup.net>
2019-07-19 19:24:22 +0200

    add pytest plugin file

---
+++ testaid/plugin.py
@@ -0,0 +1,21 @@
+import pytest
+from testaid.testpass import Testpass
+from testaid.testvars import Testvars
+
+
+@pytest.fixture
+def testpass(host):
+    return Testpass(host).testpass
+
+
+@pytest.fixture
+def testvars(request, host):
+    try:
+        cache_id = 'testvars' + os.environ['MOLECULE_EPHEMERAL_DIRECTORY']
+    except:
+        cache_id = 'testvars/global'
+    testvars = request.config.cache.get(cache_id, None)
+    if testvars is None:
+        testvars = Testvars(host).testvars
+        request.config.cache.set(cache_id, testvars)
+    return testvars
