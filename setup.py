e45f49a7f9ec4a775b2809f022ab323e914a5781
rebelcodebase <rebelcodebase@riseup.net>
2019-07-19 19:20:22 +0200

    turn testaid into a pytest plugin

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='testaid',
-        version='0.1.2',
+        version='0.2',
         author='RebelCodeBase',
         author_email='rebelcodebase@riseup.net',
         description='Fixtures for testinfra and molecule',
@@ -13,19 +13,26 @@ if __name__ == '__main__':
         url='https://github.com/RebelCodeBase/testaid',
         license='Apache-2.0',
         packages=setuptools.find_packages(),
-            install_requires=['docutils>=0.3', 'ansible>=2.8.2', 'testinfra>=3.0.5', 'molecule>=2.20.2'],
         package_data={
             '': ['*.rst'],
+        },
+        entry_points={
+            'pytest11': ['testaid=testaid.plugin']
         },
         platforms='any',
         zip_safe=False,
         classifiers=[
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
+            'Intended Audience :: Information Technology',
             'Intended Audience :: Developers',
             'Intended Audience :: System Administrators',
             'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
+            'Operating System :: POSIX',
             'Topic :: Software Development :: Libraries :: Python Modules'
+            'Topic :: Software Development :: Testing',
+            'Topic :: System :: Systems Administration',
+            'Framework :: Pytest',
+
         ],
     )
