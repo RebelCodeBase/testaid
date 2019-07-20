3bbf688a8e3fa5a574706d2c89d8916033da673e
rebelcodebase <rebelcodebase@riseup.net>
2019-07-20 16:32:30 +0200

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='testaid',
-        version='0.4',
+        version='0.4.1',
         author='RebelCodeBase',
         author_email='rebelcodebase@riseup.net',
         description='Fixtures for testinfra and molecule',
-        long_description='See [Testaid on GitHub](https://github.com/RebelCodeBase/testaid) for documentation.',
+        long_description='See https://github.com/RebelCodeBase/testaid for documentation.',
         url='https://github.com/RebelCodeBase/testaid',
         license='Apache-2.0',
         packages=setuptools.find_packages(),
