fd0c0044dfd02ef12c0479380993b1f0350f5dde
rebelcodebase <rebelcodebase@riseup.net>
2019-07-20 02:06:58 +0200

    use role default variable

--- test/debian/roles/anarchism/molecule/default/tests/test_anarchism_install.py
+++ test/debian/roles/anarchism/molecule/default/tests/test_anarchism_install.py
@@ -4,9 +4,10 @@ testinfra_hosts = testaid.hosts()
 
 
 def test_anarchism_packages_installed(host, testvars):
-    anarchism_packages = testvars['anarchism_packages']
+    if testvars['anarchism_install']:
+        anarchism_packages = testvars['anarchism_packages']
 
-    for debian_package in anarchism_packages:
-        deb = host.package(debian_package)
+        for debian_package in anarchism_packages:
+            deb = host.package(debian_package)
 
-        assert deb.is_installed
+            assert deb.is_installed
