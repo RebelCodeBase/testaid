13649761ce881062f6db0150cc1c07187e931fea
rebelcodebase <rebelcodebase@riseup.net>
2019-07-22 01:27:28 +0200

    test for different ansible_facts key

--- test/debian/molecule/default/tests/test_testaid_ansiblefacts.py
+++ test/debian/molecule/default/tests/test_testaid_ansiblefacts.py
@@ -4,4 +4,4 @@ testinfra_hosts = testaid.hosts()
 
 
 def test_testaid_ansiblefacts_present(testvars):
-    assert 'ansible_all_ipv4_addresses' in testvars['ansible_facts'].keys()
+    assert 'ansible_distribution_version' in testvars['ansible_facts'].keys()
