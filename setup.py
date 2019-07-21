f6bf3f826add90e9fa702d3d80ca4545e401c8a2
rebelcodebase <rebelcodebase@riseup.net>
2019-07-22 01:30:56 +0200

    testaid version 0.6

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='testaid',
-        version='0.5',
+        version='0.6',
         author='RebelCodeBase',
         author_email='rebelcodebase@riseup.net',
         description='Fixtures for testinfra and molecule',
-        long_description='See https://github.com/RebelCodeBase/testaid for documentation.',
+        long_description='See [testaid at github](https://github.com/RebelCodeBase/testaid) for documentation.',
         url='https://github.com/RebelCodeBase/testaid',
         license='Apache-2.0',
         packages=setuptools.find_packages(),
