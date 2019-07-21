d7d159d399301ec04ebd47a1fa0dd0c40a2662e9
rebelcodebase <rebelcodebase@riseup.net>
2019-07-22 01:35:47 +0200

    satisfy linter

--- test/debian/molecule/default/tests/test_testaid_github_issues.py
+++ test/debian/molecule/default/tests/test_testaid_github_issues.py
@@ -4,5 +4,5 @@ testinfra_hosts = testaid.hosts()
 
 # issue: https://github.com/RebelCodeBase/testaid/issues/1
 # needs: https://github.com/philpep/testinfra/pull/462
-#def test_testaid_github_issues_1(testvars):
+# def test_testaid_github_issues_1(testvars):
 #    assert testvars['github_issues_1_file'] == 'bar'
