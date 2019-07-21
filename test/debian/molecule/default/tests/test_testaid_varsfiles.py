12b93d66197a2460ab9a7f77672ebcd5f56bf5ad
rebelcodebase <rebelcodebase@riseup.net>
2019-07-21 04:27:35 +0200

    change ymlfiles to varsfiles

--- test/debian/molecule/default/tests/test_testaid_varsfiles.py
+++ test/debian/molecule/default/tests/test_testaid_varsfiles.py
@@ -3,18 +3,18 @@ import testaid
 testinfra_hosts = testaid.hosts()
 
 
-def test_testaid_ymlfiles_role_vars(testvars):
+def test_testaid_varsfiles_role_vars(testvars):
     assert testvars['my_role_var'] == 'role_var'
 
 
-def test_testaid_ymlfiles_project_vars(testvars):
+def test_testaid_varsfiles_project_vars(testvars):
     assert testvars['my_project_vars_main'] == 'project_vars_main'
 
 
-def test_testaid_ymlfiles_project_multiple_vars_files(testvars):
+def test_testaid_varsfiles_project_multiple_vars_files(testvars):
     assert testvars['my_project_vars_second'] == 'project_vars_second'
 
 
-def test_testaid_ymlfiles_extra_vars_files(testvars):
+def test_testaid_varsfiles_extra_vars_files(testvars):
     assert testvars['my_extra_vars_first'] == 'extra_vars_first'
     assert testvars['my_extra_vars_second'] == 'extra_vars_second'
