6c66051c8f99b65223d49b742a8a220a50b487b8
rebelcodebase <rebelcodebase@riseup.net>
2019-07-30 23:17:00 +0200

    Add debug code (commented out)

--- test/debian/molecule/default/tests/test_testaid_github_issues.py
+++ test/debian/molecule/default/tests/test_testaid_github_issues.py
@@ -1,8 +1,18 @@
+
+#from ruamel.yaml import YAML
+#import sys
 import testaid
 
 testinfra_hosts = testaid.hosts()
+#yaml = YAML()
 
 # issue: https://github.com/RebelCodeBase/testaid/issues/1
 # needs: https://github.com/philpep/testinfra/pull/462
-# def test_testaid_github_issues_1(testvars):
+# def test_testaid_github_issues_1_001(testvars):
 #    assert testvars['github_issues_1_file'] == 'bar'
+
+# issue: https://github.com/RebelCodeBase/testaid/issues/1
+# print testvars as yaml
+#def test_testaid_github_issues_1_002(testvars):
+#    yaml.dump(testvars, sys.stdout)
+#    assert False
