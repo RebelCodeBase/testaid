7ffa960081ea2bd97471650b0d4887669f686adc
rebelcodebase <rebelcodebase@riseup.net>
2019-08-07 14:43:07 +0200

    Add two differen ways to mock testpass

--- test/debian/molecule/default/tests/test_testaid_testpass.py
+++ test/debian/molecule/default/tests/test_testaid_testpass.py
@@ -1,14 +1,18 @@
-import pytest
+import mock
 import testaid
 
 testinfra_hosts = testaid.hosts()
 
-# this test is xfailed by default
-# as it assumes that you have done
-# pass insert my_secret_key
-# with password: my_secret_value
-# FIXME: create mock
-@pytest.mark.xfail
-def test_testaid_debug(host, testvars, testpass):
+
+@mock.patch('testaid.moleculebook.MoleculeBook.run', return_value=[{'msg':'my_secret_value'}])
+def test_testaid_testpass_mock(host, testpass):
+    my_secret_value = testpass('my_secret_key')
+    assert my_secret_value == 'my_secret_value'
+
+
+def test_testaid_testpass_mock(host, monkeypatch, testpass):
+    monkeypatch.setattr(testaid.moleculebook.MoleculeBook,
+                        'run',
+                        lambda x: [{'msg':'my_secret_value'}])
     my_secret_value = testpass('my_secret_key')
     assert my_secret_value == 'my_secret_value'
