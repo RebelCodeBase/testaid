04099e86f478ed49d34541d3ba77f63e6272fd45
rebelcodebase <rebelcodebase@riseup.net>
2019-08-07 17:42:41 +0200

    Add command line options to control testvars

--- testaid/plugin.py
+++ testaid/plugin.py
@@ -6,6 +6,35 @@ from testaid.testpass import TestPass
 from testaid.testvars import TestVars
 
 
+def pytest_addoption(parser):
+    parser.addoption("--testvars-no-gather-facts",
+        action="store_false",
+        default=True,
+        help="do not gather ansible_facts")
+    parser.addoption("--testvars-no-resolve-vars",
+        action="store_false",
+        default=True,
+        help="do not resolve jinja2 templates")
+    parser.addoption("--testvars-no-gather-molecule",
+        action="store_false",
+        default=True,
+        help="do not resolve molecule vars")
+
+@pytest.fixture(scope='session')
+def gather_facts(request):
+    return request.config.getoption("--testvars-no-gather-facts")
+
+
+@pytest.fixture(scope='session')
+def resolve_vars(request):
+    return request.config.getoption("--testvars-no-resolve-vars")
+
+
+@pytest.fixture(scope='session')
+def gather_molecule(request):
+    return request.config.getoption("--testvars-no-gather-molecule")
+
+
 @pytest.fixture(scope='session')
 def moleculeplay():
     return MoleculePlay()
@@ -22,22 +51,22 @@ def testpass(moleculebook):
 
 
 @pytest.fixture(scope='session')
-def testvars(request, moleculebook):
+def testvars(request, moleculebook, gather_facts, resolve_vars, gather_molecule):
     '''Expose ansible variables of a molecule test scenario.'''
-    # Remember: it's easier to ask for forgiveness than permission
+    # remember: it's easier to ask for forgiveness than permission
     # https://docs.python.org/3/glossary.html#term-eafp
 
-    # determine the cache key which will determine if
-    # two scenarios use the same or different cache values
+    # set the scope of the cache key
+    # MOLECULE_EPHEMERAL_DIRECTORY should be unique for each molecule scenario
     try:
-        # try to get a molecule scenario-related cache key
+        # try to get a molecule scenario-scoped cache key
         cache_key = 'testvars/' + os.environ['MOLECULE_EPHEMERAL_DIRECTORY']
     except KeyError:
         # use a global cache key as fallback
         cache_key = 'testvars/global'
 
     try:
-        # determine if cache support is enabled in molecule
+        # check if cache support is enabled in molecule
         # i.e. if the option "p: cacheprovider" is present in molecule.yml:
         # molecule -> verifier -> options
         testvars = request.config.cache.get(cache_key, None)
@@ -45,7 +74,10 @@ def testvars(request, moleculebook):
         testvars = None
 
     if testvars is None:
-        testvars = TestVars(moleculebook).get_testvars()
+        testvars = TestVars(moleculebook,
+                            resolve_vars,
+                            gather_facts,
+                            gather_molecule).get_testvars()
 
         try:
             # try to cache the testvars
