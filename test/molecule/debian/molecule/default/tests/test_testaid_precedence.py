26bfdb64301b9c6154cf301e4bd7c0984df84724
rebelcodebase <rebelcodebase@riseup.net>
2019-08-08 17:04:19 +0200

    Add test cases

--- test/molecule/debian/molecule/default/tests/test_testaid_precedence.py
+++ test/molecule/debian/molecule/default/tests/test_testaid_precedence.py
@@ -9,5 +9,4 @@ def test_testaid_precedence_can_read_role_default(host, testvars):
 
 def test_testaid_precedence_project_beats_role_default(host, testvars):
     expected = ['anarchism', 'fortune-anarchism']
-
     assert testvars['anarchism_packages'] == expected
--- test/molecule/debian/molecule/default/tests/test_testaid_varsfiles.py
+++ test/molecule/debian/molecule/default/tests/test_testaid_varsfiles.py
@@ -3,21 +3,21 @@ import testaid
 testinfra_hosts = testaid.hosts()
 
 
+def test_testaid_varsfiles_role_defaults(host, testvars):
+    assert testvars['my_role_defaults_var'] == 'my_role_defaults_value'
+
+
 def test_testaid_varsfiles_role_vars(host, testvars):
     assert testvars['my_role_var'] == 'my_role_value'
 
 
-def test_testaid_varsfiles_role_vars(host, testvars):
-    assert testvars['my_project_var'] == 'my_project_value2'
+def test_testaid_varsfiles_project_vars(host, testvars):
+    assert testvars['my_project_var'] == 'my_project_value'
 
 
-def test_testaid_varsfiles_role_vars(host, testvars):
+def test_testaid_varsfiles_extra_vars(host, testvars):
     assert testvars['my_extra_var'] == 'my_extra_value'
 
 
-def test_testaid_varsfiles_role_vars(host, testvars):
-    assert testvars['my_second_extra_var'] == 'my_second_extra_value'
-
-
-def test_testaid_varsfiles_role_vars(host, testvars):
+def test_testaid_varsfiles_second_file_in_vars_dir(host, testvars):
     assert testvars['my_custom_var'] == 'my_custom_value'
