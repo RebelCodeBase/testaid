9bf46da40834c3ff6831e0b4aaf975273793750d
rebelcodebase <rebelcodebase@riseup.net>
2019-08-10 04:02:24 +0200

    Add minimal playbook run test

--- test/unit/test_testaid_moleculeplay.py
+++ test/unit/test_testaid_moleculeplay.py
@@ -4,3 +4,12 @@ def test_testaid_moleculeplay_is_not_none(moleculeplay):
 
 def test_testaid_moleculeplay_get_host(moleculeplay):
     assert moleculeplay.get_host() == moleculeplay._host
+
+
+def test_testaid_moleculeplay_run_playbook_minimal(moleculeplay):
+    playbook_minimal = \
+        {'name': 'ansible minimal playbook',
+         'hosts': 'localhost',
+         'gather_facts': 'False'}
+    result = moleculeplay.run_playbook(playbook_minimal)
+    assert result == []
