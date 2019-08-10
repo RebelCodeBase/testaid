d77c35d5aca07cc22041487c96a44a5fbaee1c1a
rebelcodebase <rebelcodebase@riseup.net>
2019-08-10 19:43:47 +0200

    testaid/exceptions.py

--- test/unit/test_testaid_moleculeplay.py
+++ test/unit/test_testaid_moleculeplay.py
@@ -1,9 +1,31 @@
+import pytest
+from testaid.exceptions import MoleculePlayRunFailed
+
+
 def test_testaid_moleculeplay_is_not_none(moleculeplay):
     assert moleculeplay is not None
 
 
 def test_testaid_moleculeplay_get_host(moleculeplay):
     assert moleculeplay.get_host() == moleculeplay._host
+
+
+def test_testaid_moleculeplay_exception_moleculeplayrunfailed_no_debug():
+    result = ['my_result']
+    msg = 'my_msg'
+    with pytest.raises(
+            MoleculePlayRunFailed,
+            match=r'^my_msg$'):
+        raise MoleculePlayRunFailed(result, msg)
+
+
+def test_testaid_moleculeplay_exception_moleculeplayrunfailed_debug():
+    result = ['my_result']
+    msg = 'my_msg'
+    with pytest.raises(
+            MoleculePlayRunFailed,
+            match=r'.*my_result.*'):
+        raise MoleculePlayRunFailed(result, msg, debug=True)
 
 
 def test_testaid_moleculeplay_run_playbook_minimal(moleculeplay):
