9a89f06b05c2c0ec937a5236b5725196aee3c727
rebelcodebase <rebelcodebase@riseup.net>
2019-08-12 20:03:11 +0200

    Use local variables instead of instance variables

--- testaid/testvars.py
+++ testaid/testvars.py
@@ -16,19 +16,13 @@ class TestVars(object):
                  gather_facts,
                  extra_vars):
 
-        # use moleculebook fixture to get vars
-        self._moleculebook = moleculebook
-
-        # use jsonvars fixture to resolve json vars
-        self._jsonvars = jsonvars
-
         # this variable will be returned by the testvars fixture
         self._testvars = dict()
 
         # get ansible variables
-        testvars_unresolved = self._moleculebook.get_vars(resolve_vars,
-                                                          gather_facts,
-                                                          extra_vars)
+        testvars_unresolved = moleculebook.get_vars(resolve_vars,
+                                                    gather_facts,
+                                                    extra_vars)
 
         if not resolve_vars:
 
@@ -38,19 +32,19 @@ class TestVars(object):
         else:
 
             # convert python variables to json
-            self._testvars_unresolved_json = json.dumps(testvars_unresolved)
+            testvars_unresolved_json = json.dumps(testvars_unresolved)
 
             # set jsonvars to unresolved testvars
-            self._jsonvars.set(self._testvars_unresolved_json)
+            jsonvars.set(testvars_unresolved_json)
 
             # resolve unresolved json testvars
-            self._jsonvars.resolve()
+            jsonvars.resolve()
 
             # get resolved testvars from jsonvars
-            self._testvars_resolved_json = self._jsonvars.get()
+            testvars_resolved_json = jsonvars.get()
 
             # convert json vars to python
-            self._testvars = json.loads(self._testvars_resolved_json)
+            self._testvars = json.loads(testvars_resolved_json)
 
     def get_testvars(self):
         return self._testvars
