169953f5bf2be66fc5fcf0621ce5847f9c636e15
rebelcodebase <rebelcodebase@riseup.net>
2019-08-15 04:50:15 +0200

    v0.9.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='testaid',
-        version='0.9.5',
+        version='0.9.6',
         author='RebelCodeBase',
         author_email='rebelcodebase@riseup.net',
         description='Fixtures for ansible, testinfra and molecule',
