c5792e9777cde74bd85b82b053973a231a5b9a5c
rebelcodebase <rebelcodebase@riseup.net>
2019-08-15 13:23:01 +0200

    Add unit test for testvars

--- test/unit/test_testaid_unit_testvars.py
+++ test/unit/test_testaid_unit_testvars.py
@@ -1,6 +1,6 @@
 import pytest
+import testaid
 from testaid.testvars import TestVars
-from testaid.exceptions import TemplatesResolveError
 from testaid.exceptions import AnsibleRunError
 
 
@@ -30,3 +30,34 @@ def test_testaid_unit_testvars_no_resolve_vars(
                         gather_facts,
                         extra_vars).get_testvars()
     assert 'inventory_hostname' in testvars
+
+
+def test_testaid_unit_testvars_resolve_vars(
+        moleculebook,
+        jsonvars,
+        monkeypatch):
+
+    testvars_unresolved = \
+        {'my_var_1': 'my_value', 'my_var_2': '{{ my_var_1 }}'}
+    jsonvars_resolved = \
+        '{"my_var_1": "my_value", "my_var_2": "my_value"}'
+    testvars_resolved = \
+        {'my_var_1': 'my_value', 'my_var_2': 'my_value'}
+    monkeypatch.setattr(testaid.moleculebook.MoleculeBook,
+                        'get_vars',
+                        lambda w, x, y, z: testvars_unresolved)
+    monkeypatch.setattr(testaid.jsonvars.JsonVars,
+                        'resolve',
+                        lambda x: None)
+    monkeypatch.setattr(testaid.jsonvars.JsonVars,
+                        'get',
+                        lambda x: jsonvars_resolved)
+    resolve_vars = True
+    gather_facts = False
+    extra_vars = ''
+    testvars = TestVars(moleculebook,
+                        jsonvars,
+                        resolve_vars,
+                        gather_facts,
+                        extra_vars).get_testvars()
+    assert testvars == testvars_resolved
