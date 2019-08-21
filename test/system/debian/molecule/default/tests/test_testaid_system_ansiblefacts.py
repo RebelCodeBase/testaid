81bf9dfc8f368e321f69e2fa32c6f5b20f0a8b10
rebelcodebase <rebelcodebase@riseup.net>
2019-08-22 00:44:00 +0200

    Prepare ansible_facts template

--- test/system/debian/molecule/default/tests/test_testaid_system_ansiblefacts.py
+++ test/system/debian/molecule/default/tests/test_testaid_system_ansiblefacts.py
@@ -5,3 +5,7 @@ testinfra_hosts = testaid.hosts()
 
 def test_testaid_system_ansiblefacts_present(host, testvars):
     assert 'ansible_distribution_version' in testvars['ansible_facts']
+
+
+#def test_testaid_system_ansiblefacts_reference(host, testvars):
+#    assert testvars['factref'] is not ''
