2fd1195366405ea91c4933c02ff1e17e2c7a10b2
rebelcodebase <rebelcodebase@riseup.net>
2019-11-26 15:19:57 +0100

    Refactor system tests

--- test/system/ruby/molecule/whitelist/tests/test_testaid_system_ruby_project_whitelist.py
+++ test/system/ruby/molecule/whitelist/tests/test_testaid_system_ruby_project_whitelist.py
@@ -4,7 +4,7 @@ testinfra_hosts = testaid.hosts()
 
 
 def test_testaid_ruby_project_test_whitelist_curl_and_vim(host, testvars):
-    assert 'curl_packges' in testvars
+    assert 'curl_packages' in testvars
     assert 'vim_packages' in testvars
     assert 'gpg_packages' not in testvars
-    assert  'procps_packages' not in testvars
+    assert 'procps_packages' not in testvars
