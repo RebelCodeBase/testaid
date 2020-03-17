2332572ecd636a45743ddf076bdf4e3f420264bf
rebelcodebase <rebelcodebase@riseup.net>
2020-03-17 23:48:32 +0100

    Add new reference tests

--- test/system/anarchism/molecule/default/tests/test_testaid_system_anarchism_project_group.py
+++ test/system/anarchism/molecule/default/tests/test_testaid_system_anarchism_project_group.py
@@ -4,4 +4,5 @@ testinfra_hosts = testaid.hosts()
 
 
 def test_testaid_system_project_group(host, testvars):
-    assert testvars['anarchism_group_vars_private'] == 'my_group_vars_private_value'
+    assert testvars['anarchism_group_vars_private_var'] \
+           == 'my_group_vars_private_value'
